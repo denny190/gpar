3bbc1871c932a96099fcce24536b7b89b30105ed
denny190 <daniel@berdar.cz>
2022-11-09 13:09:32 +0100

    modified:   bondlength_project/bondlength.sh 	modified:   bondlength_project/logparser.py 	deleted:    bondlength_project/paths.txt.tmp 	new file:   bondlength_project/tmp/paths.txt.tmp 	new file:   bondlength_project/tmp/selected_job_paths.txt.tmp

--- bondlength_project/bondlength.sh
+++ bondlength_project/bondlength.sh
@@ -7,59 +7,63 @@ tmp_dir=$cwd/tmp
 
 mkdir -p $cwd/tmp $cwd/bond_out
 
-# ?? Specify bonds?
+# Specify bonds?
 
-# Specify dir in which all .log or subdirs with .log files are located
-echo "[PROMPT] Input the path of your directory in which .log files (or subdirs with .log files) are located. Type -1 to exit."
-read -e searched_dir
-
-###Checking whether the inputted file exists. 
-###Prevention of submitting non-existent files in case of typos etc.
-	###	THIS IS WRONG!!
-until test -d "$searched_dir" || [ $searched_dir == -1 ]
+# Takes user input. If everything is ok exits loop and continues execution. If not, prompts user again until input correct or user quits
+while :
 do
-	echo "[ERROR] Could not locate '~/$searched_dir'."
 	echo "[PROMPT] Input the path of your directory in which .log files (or subdirs with .log files) are located. Type -1 to exit."
-	read -e searched_dir
+	read -e path_input
+
+	if test -d "$path_input" 
+	then
+		echo "[INFO] Succesfully located $path_input."
+
+		find $path_input -name *.log > $tmp_dir/paths.txt.tmp
+
+		num_logfiles=$(wc -l < $tmp_dir/paths.txt.tmp)
+		echo "[INFO] Found $num_logfiles log files."
+
+		if [ $num_logfiles -eq 0 ]
+		then
+			echo "[ERROR] No logfiles found. Try again."
+			continue
+		fi
+		
+		break
+
+	elif [ $path_input -eq -1 ]
+	then
+		echo "[EOF] EXITING."
+		exit 0
+
+	else
+		echo "[ERROR] Could not locate $path_input" 
+	fi
 done
-
-if [ $searched_dir == -1 ]; then
-	echo "[EOF] EXITING."
-	exit 0
-fi
-# Save paths of selected .log files in a paths.txt.tmp file
-find $inpDir -name *.log > paths.txt.tmp
-path_file=$tmp_dir/paths.txt.tmp
-
-num_logfiles=$(wc -l < $tmp_dir/paths.txt.tmp)
-echo "[INFO] Found $num_logfiles log files."
-
-# If no files were found exit (sort this out so user can make another attempt if = 0
-if [ $num_logfiles == 0 ]; then
-	echo "[EOF] No log files found. EXITING."
-	exit 1
-fi
 
 # Specify what jobs to take geom from? (ie. only opt jobs)
 ### NOT NECESSARY NOW
 job_type="FOpt"
+[ ! -e $tmp_dir/selected_job_paths.txt.tmp ] || rm $tmp_dir/selected_job_paths.txt.tmp
 
 # For loop to go to each file, extract information and coordinates into a separate file for each functional
-for line in $path_file; do
+for line in $(cat $tmp_dir/paths.txt.tmp)
+	do
+	# Extract filename from path
+	file_name="$(basename "$line")"
 
 	# Checks whether the log file is for an OPT job - if not, skips the iteration
-	if grep -q $job_type $line; then
-		$line > $tmp_dir/selected_job_paths.txt.tmp
+	if grep -q $job_type $line
+	then
+		echo "$line" >> $tmp_dir/selected_job_paths.txt.tmp
+
+		#Start python code that parses each log file
+		python logparser.py $line $file_name
+		
 	else
 		continue
 	fi
-	
-	#logparser.py $line $
 
 done
 
-# Calculate bond lengths
-
-# Group data from same functionals together
-
-# Optionally calculate differences in bond lengths
--- bondlength_project/logparser.py
+++ bondlength_project/logparser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import os
 import re
 
 ###Passed arguments from bondlength.sh
@@ -11,8 +12,9 @@ file_name = sys.argv[2]
 header_line_nums = []
 footer_line_nums = []
 
-archive_header = r'1\1'
-archive_footer = r'\\@'
+archive_header = r"1\1"
+archive_footer = "\\@"
+job_type = "FOpt"
 
 ###Open the logfile
 with open(file_path) as f:
@@ -20,19 +22,32 @@ with open(file_path) as f:
 
 ###Find number of archive entries and identify lines where headers and footers are located
 for line in lines:
-    if line.find(archive_header):
+    if archive_header in line:
         header_line_nums.append(lines.index(line))
 
-    if line.find(archive_footer):
+    if archive_footer in line:
         footer_line_nums.append(lines.index(line))
 
-###If the num
+#
 if (len(header_line_nums) != len(footer_line_nums)):
-    print(("[ERROR] Number of archiv headers and footers is not equal. Please manually check whether logfile %s is written out correctly!") % (file_path))
+    print(("[ERROR] Number of archive headers and footers is not equal. Please manually check whether logfile %s is written out correctly!") % (file_path))
     print("[EOF] EXITING.")
     exit(1)
 
 ###For each instance of archive entry into the file split each info tab (denomitated by \) into a separate newline
 for job in range(len(header_line_nums)):
-    whole_archive = lines[archive_header:archive_footer]
-    split_archive = whole_archive.split(r'/')
+    whole_archive = lines[header_line_nums[job]:footer_line_nums[job]]
+
+    #If first line of archive contains the desired job tag
+    if job_type in whole_archive[0]:
+        ###Strip newline characters from end of each line
+        whole_archive = [item.strip() for item in whole_archive]
+        #Join all elems in whole_archive to allow splitting via \ as denominator (havent figured out a better way to do this)
+        string_archive = ''.join(whole_archive)
+
+        split_archive = string_archive.split("\\")
+
+        for x in split_archive:
+            print(x)
+    else:
+        continue
