6b6609034208cebe78a153ae54bc4d12c7a63c02
denny190 <daniel@berdar.cz>
2023-05-28 00:53:24 +0200

    modified:   bondlength_project/pylog2.py 	new file:   bondlength_project/pylog_prod.py

--- bondlength_project/pylog_prod.py
+++ bondlength_project/pylog_prod.py
@@ -34,26 +34,25 @@ def parse(infile):
     regexes = {
         'version': re.compile(r'Cite this work as:'),
         'chk': re.compile(r'^%chk=(\S+)'),
-        'route': re.compile(r'^#'),
+        'archive': re.compile(r'^1\\1'),
         'hfenergy': re.compile(r'^SCF Done:'),
         'input': re.compile(r'^Symbolic Z-matrix:'),
-        'tddft': re.compile(r'Convergence achieved on expansion vectors.'),
-        'tddft_root_info': re.compile(r'^(This state)|^(Total Energy)|^(Copying)'),
         'freq': re.compile(r'^Harmonic frequencies \(cm\*\*-1\)'),
-        'hessian': re.compile(r'^(The second derivative matrix:)|^(ITU=  0)'),
         'opt': re.compile(r'Stationary point found.'),
         'force': re.compile(r'Center     Atomic                   Forces'),
         'cpu_time': re.compile(r'^Job cpu time:'),
-        'termination': re.compile(r'(?:Normal termination of Gaussian \d+ at )(.*)\.')
+        'termination': re.compile(r'(Normal termination of Gaussian \d+ at )(.*)\.')
     }
 
     parsed = {'type': 'g16'}
+
     with infile as logfile:
         for line in logfile:
             line = line.strip()
 
             #TODO: Implement option passing from .sh script HERE
-            re_order = ['version', 'chk', 'route', 'input','hfenergy', 'opt', 'freq', 'hessian', 'tddft', 'force', 'termination']
+            re_order = ['version', 'chk', 'input', 'archive', 'hfenergy', 'opt', 'force', 'termination']
+            #re_order = ['archive']
 
             for regex in re_order:
                 try:
@@ -65,17 +64,28 @@ def parse(infile):
                     if regex == 'version':
                         parsed['version'] = next(
                             logfile).strip().replace(',', '')
+                        
                     elif regex == 'chk':
                         parsed['chk'] = match.group(1).replace('.chk', '') + '.chk'
-                    elif regex == 'route':
-                        parsed['route'] = [line[1:].strip()]
+
+                    elif regex == 'archive':
+                        archive_array = []
+                        archive_array.append(line[1:].strip())
                         try:
                             line = next(logfile).strip()
                             while line[0:3] != '---':
-                                parsed['route'].append(line.strip())
+                                archive_array.append(line.strip())
                                 line = next(logfile).strip()
+                                
+                                #TODO: Filter out DipoleDeriv= and PolarDeriv= from archive as the output is unnecessarily large and useless
+                                
                         except StopIteration:
                             pass
+                        
+                        joined_archive = ''.join(archive_array)
+                        clean_archive = joined_archive.split("\\")
+                        parsed['archive'] = clean_archive
+
                     elif regex == 'input':
                         line = next(logfile).strip()
                         parsed['input'] = {'charge': line.split()[2], 'multi': line.split()[5],
@@ -89,6 +99,7 @@ def parse(infile):
 
                     elif regex == 'hfenergy':
                         parsed['energy'] = float(line.split()[4])
+
                     elif regex == 'opt':
                         parsed['opt'] = {'success': True, 'geom': []}
                         geom = True
@@ -107,14 +118,16 @@ def parse(infile):
                                 parsed['opt']['geom'].append(
                                     [int(i) for i in line.split()[0:3]] + [float(i) for i in line.split()[3:]])
                                 line = next(logfile).strip()
+
                     elif regex == 'freq':
-                        continue
                         freq = True
+
                         while line.split()[0] != '1':
                             line = next(logfile).strip()
                             if not line:
                                 freq = False
                                 break
+
                         if freq:
                             while line.split():
                                 freq_parsed = line.split()
@@ -154,95 +167,7 @@ def parse(infile):
                                         'ir_intensity': float(ir_inten[mode]),
                                         'mode': nmodes[mode]
                                     }
-                    elif regex == 'hessian':
-                        parsed['hessian'] = parsed.setdefault('hessian', {})
-
-                        if match.group(1):
-                            line = next(logfile).strip()
-                            raw_matrix = []
-                            while not line.startswith('ITU='):
-                                line = next(logfile).strip()
-                                submatrix = []
-                                while (isfloat(line.split()[-1]) and
-                                       not line.startswith('ITU=')):
-                                    submatrix.append([float(i)
-                                                      for i in line.split()[1:]])
-                                    line = next(logfile).strip()
-                                raw_matrix.append(submatrix)
-                            matrix = raw_matrix[0]
-                            for submatrix in raw_matrix[1:]:
-                                for i, v in enumerate(reversed(submatrix)):
-                                    matrix[-i-1] += v
-                            parsed['hessian']['matrix'] = matrix
-
-                        parsed['hessian']['eigenvalues'] = []
-                        line = next(logfile).strip()
-                        while line.startswith('Eigenvalues ---'):
-                            parsed['hessian']['eigenvalues'] += [
-                                float(i) for i in line.split()[2:]]
-                            line = next(logfile).strip()
-                    elif regex == 'tddft':
-                        parsed['tddft'] = {}
-                        for _ in range(4):
-                            next(logfile)
-                        line = next(logfile).strip()
-                        while (line.split() and 'electric dipole moments' not in line):
-                            line = next(logfile).strip()
-                        next(logfile)
-
-                        parsed['tddft'] = {'electric_dipole_moments': []}
-                        line = next(logfile).strip()
-                        while (line.split() and line.split()[0].isdigit()):
-                            parsed['tddft']['electric_dipole_moments'].append(
-                                line.split()[1:])
-                            line = next(logfile).strip()
-
-                        while (line[0:3] != '***' and 'Excitation energies and oscillator strengths:' not in line):
-                            line = next(logfile).strip()
-                        next(logfile)
-                        line = next(logfile).strip()
-                        parsed['tddft']['excited_states'] = []
-                        
-                        while line.startswith('Excited State'):
-                            break
-                            venergy = float(line.split()[4])
-                            osc_strength = float(line.split()[8][2:])
-                            total_energy = None
-                            contribs = []
-
-                            line = next(logfile).strip()
-                            while (line.split() and line.split()[0].isdigit()):
-                                #TODO: Fix Excited States!
-                                print(line)
-                                contrib = line.split()
-                                del contrib[1]
-
-                                if len(contrib) >= 3:
-                                    contrib = [
-                                        int(float(i)) for i in contrib[:2]] + [float(contrib[2])]
-                                    contribs.append(contrib)
-                                else:
-                                    line = next(logfile).strip()
-                                    break  # Added break condition
-
-                                line = next(logfile).strip()
-                            root_match = regexes['tddft_root_info'].search(
-                                line)
-                            while root_match:
-                                if root_match.group(2):
-                                    total_energy = float(line.split()[4])
-                                line = next(logfile).strip()
-                                root_match = regexes['tddft_root_info'].search(
-                                    line)
-
-                            parsed['tddft']['excited_states'].append({
-                                'excitation_energy': venergy,
-                                'oscillator_strength': osc_strength,
-                                'contributions': contribs,
-                                'total_energy': total_energy
-                            })
-
-                            line = next(logfile).strip()
+        
                     elif regex == 'force':
                         line = next(logfile)
                         line = next(logfile)
@@ -253,12 +178,16 @@ def parse(infile):
                             parsed['force'].append([int(i) for i in line.split()[
                                                    0:2]] + [float(i) for i in line.split()[2:]])
                             line = next(logfile).strip()
+
                     elif regex == 'cpu_time':
                         cpu_time = line.split()[3:]
                         parsed['cpu_time'] = float(cpu_time[0])*24*60*60 + \
                             float(cpu_time[2])*60*60 + \
                             float(cpu_time[4])*60 + float(cpu_time[8])
+                        
                     elif regex == 'termination':
+                        print("DEBUG TERMINATION!!!!!!!!!!!!!!!!!!!!!!!!!")
+                        print("TERMINATION" + line)
                         parsed['termination'] = {'normal': True,
                                          'time': datetime.strptime(' '.join(match.group(1).split()),
                                                                    '%a %b %d %H:%M:%S %Y')}
@@ -267,8 +196,10 @@ def parse(infile):
     try:
         if parsed['opt']['success'] and not parsed['opt']['geom']:
             del parsed['opt']
+
     except KeyError:
         pass
+
     if 'termination' not in parsed:
         parsed['termination'] = {'normal': False}
 
@@ -288,7 +219,8 @@ for line in logpaths:
 
 for file in log_files:
     with open(file, 'r') as logfile:
+        parse(logfile)
         #print(parse(logfile))
-        print_dict(parse(logfile))
+        #print_dict(parse(logfile))
         
 
